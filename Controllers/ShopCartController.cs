492a34780c713cfdc492756323f82e461cebc15e
jayjay.meow <jayjay.meow@gmail.com>
2022-05-23 00:12:43 +0300

    Fix namespace errors

--- Controllers/ShopCartController.cs
+++ Controllers/ShopCartController.cs
@@ -2,7 +2,7 @@
 using CarShop.Data.Models;
 using CarShop.Pages;
 using Microsoft.AspNetCore.Mvc;
-using Shop.ViewsModels;
+using CarShop.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- ViewModels/ShopCartViewModel.cs
+++ ViewModels/ShopCartViewModel.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Shop.ViewsModels
+namespace CarShop.ViewModels
 {
     public class ShopCartViewModel
     {
